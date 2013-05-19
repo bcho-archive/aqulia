74260bfef24ed0804341447b32d94538e32c5fa0
hbc <bcxxxxxx@gmail.com>
2013-05-19 16:40:01 +0800

    added str utils

---
+++ src/utils.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <ctype.h>
+
+char *trim(char *str)
+{
+    char *left, *right, *p;
+
+    left = str;
+    right = str + strlen(str) - 1;
+
+    while (isspace(*left))
+        left++;
+    while (isspace(*right))
+        right--;
+
+    if (left == str) {
+        *(right + 1) = '\0';
+    } else {                    /* shift the block forward */
+        p = str;
+        while (left <= right)
+            *p++ = *left++;
+
+        *p = '\0';
+    }
+
+    return str;
+}
+
+char *split(char *str, char delim)
+{
+    char *p;
+
+    p = strchr(str, delim);
+
+    if (p != NULL) {
+     *p = '\0';
+     return p + 1;
+    }
+    return NULL;
+}
---
+++ src/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/* trim the space */
+char *trim(char *str);
+
+/* 
+ * split string into two part with given delimiter
+ * 
+ * return value: the right part or NULL if delimiter not found
+ */
+char *split(char *str, char delim);
+
+#endif
